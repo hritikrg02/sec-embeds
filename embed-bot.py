52fdeb4f109f8aeabe2e35d645a3e8bfee75cfee
Ricky Gupta <hritikrg02@gmail.com>
2025-02-23 12:01:27 -0500

    changed file name to main.py, added docs

--- embed-bot.py
+++
@@ -1,3 +0,0 @@
-import discord
-from discord.ext import commands
-from loguru import logger
---
+++ main.py
@@ -0,0 +1,7 @@
+# file:         main.py
+# description:  contains the main implementation for the bot.
+# author:       Hritik "Ricky" Gupta | hritikrg02@gmail.com
+
+import discord
+from discord.ext import commands
+from loguru import logger
