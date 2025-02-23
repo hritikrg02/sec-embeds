389b369af80c08f468b0e29cd33d6269e3cc1f2a
Ricky Gupta <hritikrg02@gmail.com>
2025-02-23 12:03:10 -0500

    created basic stub for bot

--- main.py
+++ main.py
@@ -5,3 +5,25 @@
 import discord
 from discord.ext import commands
 from loguru import logger
+
+from utils import get_token
+
+# constants
+
+TOKEN_FILE = "bot_root/token.txt"
+TOKEN = get_token(TOKEN_FILE)
+
+# discord stuff
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    logger.success(f"Log on successful as {bot.user}.")
+
+bot.run(TOKEN)
